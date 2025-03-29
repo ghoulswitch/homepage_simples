c00847f68976285fc54c13b7978d64c3dfef7008
ghoulswitch <die.millyturn@gmail.com>
2025-03-29 14:49:57 -0300

    fix: logo alarado no modo escuro & claro

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const darkMode = localStorage.getItem("dark-mode");
     const botaoLuaLight = document.getElementById("icon-lua-claro");
     const botaoLuaDark = document.getElementById("icon-lua-escuro");
+    const logoDark = document.getElementById("logo-dark");
+    const logoLight = document.getElementById("logo-light");
 
     const enableDarkMode = () => {
         document.body.classList.add('dark-mode')
@@ -19,8 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const darkMode = localStorage.getItem("dark-mode");
         if (darkMode !== "active") {
         enableDarkMode();
+        logoDark.style.display = 'block'; // Mostra o logo escuro
+        logoLight.style.display = 'none';
         } else {
         disableDarkMode()
+        logoDark.style.display = 'none'; // Esconde o logo escuro
+        logoLight.style.display = 'block';
         }
     });
 
@@ -28,10 +34,22 @@ document.addEventListener("DOMContentLoaded", () => {
         const darkMode = localStorage.getItem("dark-mode");
         if (darkMode !== "active") {
         enableDarkMode();
+        logoDark.style.display = 'block'; // Mostra o logo escuro
+        logoLight.style.display = 'none';
         } else {
         disableDarkMode()
+        logoDark.style.display = 'none'; // Esconde o logo escuro
+        logoLight.style.display = 'block';
         }
     });
-
 });
+
+
+
+
+
+
+
+
+
 
